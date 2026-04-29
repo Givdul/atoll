b08b97068687a2d39582bfee1fe6087351f46a3d
ludvighansen <lubuha@hotmail.com>
2026-04-29 21:45:39 +0200

    Initial Commit

---
+++ Atoll/AtollApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtollApp.swift
+//  Atoll
+//
+//  Created by Ludvig Hansen on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct AtollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
